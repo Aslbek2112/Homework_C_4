13471c757a126a48f7461f09f37a3fa2ede8186a
Mahmiev Aslbek <mahmiyev.aslbek@gmail.com>
2023-06-27 15:46:11 +0300

    Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 29.
+Напишите программу, которая задает массив из 8 элементов и выводит их на экран.
+*/
+Console.Clear();
+int[] array = new int[8]; 
+Random random = new Random(); 
+        
+
+for (int i = 0; i < array.Length; i++) 
+{
+    array[i] = random.Next(10);
+}
+        
+
+Console.WriteLine("Массив:");
+for (int i = 0; i < array.Length; i++) 
+{
+    Console.Write(array[i] + " ");
+}
